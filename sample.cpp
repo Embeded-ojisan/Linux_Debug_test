bf2f79a2b9b2e8a74d679322cc9075c1c48caff0
Local PC <lowlevelenginner@gmail.com>
2023-01-18 23:20:06 +0900

    origin

---
+++ sample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+    int a = 1;
+
+    if(3==a)
+    {
+        std::cout << "HHH" << std::endl;
+    }
+    else
+    {
+        std::cout << "HHH" << std::endl;
+    }
+
+    std::cout << "HHH" << std::endl;
+}
