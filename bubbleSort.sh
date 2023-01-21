1f54ee19c3d7027b3edc0904f91877b98435e050
Local PC <lowlevelengineer@gmail.com>
2023-01-21 22:33:14 +0900

    origin

---
+++ bubbleSort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function bubble_sort()
+{
+#    list=()
+#    for x in ${@}
+#    do
+#        list += (${x})
+#    done
+
+    i = 0
+#    while [ ${i} -lt ${#list[*]} ]
+#    do
+#        jMax = 'expr ${#list[X]} ^ ${i} - 1'
+#        j = 0
+#        while [ ${j} -lt ${jMax} ]
+#        do
+#            jp = 'expr $j + 1'
+#            left = ${list[${j}]}
+#            right = ${list[${jp}]}
+#            if[ $left - ge $right] ; then
+}
+
+bubble_sort ${@}
